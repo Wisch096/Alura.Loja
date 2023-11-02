08f57f6e4af14cb80dbeeed430fcf90741e1a327
Wisch096 <wisch.pg@outlook.com>
2023-11-02 20:15:46 -0300

    Feat: adicionando a injeção de dependencias no context da loja

--- Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -10,20 +10,22 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            GravarUsandoAdoNet();
+            GravarUsandoEntity();
         }
 
-        private static void GravarUsandoAdoNet()
+        private static void GravarUsandoEntity()
         {
             Produto p = new Produto();
             p.Nome = "Harry Potter e a Ordem da Fênix";
             p.Categoria = "Livros";
             p.Preco = 19.89;
 
-            using (var repo = new ProdutoDAO())
+            using (var contexto = new LojaContext())
             {
-                repo.Adicionar(p);
+                contexto.Produtos.Add(p);
+                contexto.SaveChanges();
             }
         }
+
     }
 }
